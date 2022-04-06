51f5ac2cb449ef5fc7fe49cd72f491c3f757b4a5
Simon Nußbaumer <770632+Ponjimon@users.noreply.github.com>
2022-04-06 13:56:20 +0200

    Revert "feat: add emotion"
    This reverts commit c9b541735b5c1396d1eaf601d97021689eb0db61.

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,31 +1,4 @@
-import { CacheProvider } from '@emotion/react';
-import { useCallback, useState } from 'react';
 import { hydrate } from 'react-dom';
 import { RemixBrowser } from 'remix';
-import ClientStyleContext from './styles/client.context';
-import createEmotionCache from './styles/createEmotionCache';
 
-type ClientCacheProviderProps = {
-  children: React.ReactNode;
-};
-
-function ClientCacheProvider({ children }: ClientCacheProviderProps) {
-  const [cache, setCache] = useState(createEmotionCache());
-
-  const reset = useCallback(() => {
-    setCache(createEmotionCache());
-  }, []);
-
-  return (
-    <ClientStyleContext.Provider value={{ reset }}>
-      <CacheProvider value={cache}>{children}</CacheProvider>
-    </ClientStyleContext.Provider>
-  );
-}
-
-hydrate(
-  <ClientCacheProvider>
-    <RemixBrowser />
-  </ClientCacheProvider>,
-  document
-);
+hydrate(<RemixBrowser />, document);
